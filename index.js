d9c8aff63def418bb289709da41921ca9f4ea258
Krutika patel <krutikakamli@gmail.com>
2024-07-14 00:18:10 +0530

    added code

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ const response = require('./src/constants/response')
 app.get('/oauth/authorize', async (req, res) => {
   try {
     const querystring = require('querystring');
-   const redirectURI = querystring.escape(process.env.REDIRECT_URI)
-    const authURL = `https://api.instagram.com/oauth/authorize?client_id=${process.env.INSTA_APP_ID}&redirect_uri=${redirectURI}&scope=user_profile,user_media&response_type=code`;
+  //  const redirectURI = querystring.escape(process.env.REDIRECT_URI)
+    const authURL = `https://api.instagram.com/oauth/authorize?client_id=${process.env.INSTA_APP_ID}&redirect_uri=${process.env.REDIRECT_URI}&scope=user_profile,user_media&response_type=code`;
     // res.redirect(authURL);
     return response(res, true, 200, 'Success', authURL)
   } catch (error) {
@@ -40,46 +40,95 @@ app.get('/oauth/authorize', async (req, res) => {
   }
 });
 
-app.get('/instaCallback', async(req, res) => {
-  try {
-    const { code } = req.query;
-    console.log(process.env.REDIRECT_URI);
-    console.log(req.query, ' ---- code 66 ----');
-    const tokenURL = "https://api.instagram.com/oauth/access_token";
-    const client_id = process.env.INSTA_APP_ID;
-    const client_secret = process.env.INSTA_APP_SECRET;
+// app.get('/instaCallback', async(req, res) => {
+//   try {
+//     const { code } = req.query;
+//     console.log(process.env.REDIRECT_URI);
+//     console.log(req.query, ' ---- code 66 ----');
+//     const tokenURL = "https://api.instagram.com/oauth/access_token";
+//     const client_id = process.env.INSTA_APP_ID;
+//     const client_secret = process.env.INSTA_APP_SECRET;
      
-      const querystring = require('querystring')
-      // const redirectUri = querystring.escape(process.env.REDIRECT_URI)
-      const redirectUri = 'https://localhost:2020/instaCallback'
+//       const querystring = require('querystring')
+//       // const redirectUri = querystring.escape(process.env.REDIRECT_URI)
+//       const redirectUri = 'https://localhost:2020/instaCallback'
       
 
-      axios.post(tokenURL, {
-        client_id: process.env.INSTA_APP_ID,
-        client_secret: process.env.INSTA_APP_SECRET,
-        grant_type: "authorization_code",
-        redirect_uri: process.env.REDIRECT_URI,
-        code: req.query.code,
-      }, {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      })
-      .then(resData => {
-        return `Access Token: ${resData.data.access_token}`;
-      })
-      .catch(error => {
-        console.log(error, '-- error 109 ----');
-        return "Error fetching access token";
-      })
+//       axios.post(tokenURL, {
+//         client_id: process.env.INSTA_APP_ID,
+//         client_secret: process.env.INSTA_APP_SECRET,
+//         grant_type: "authorization_code",
+//         redirect_uri: process.env.REDIRECT_URI,
+//         code: req.query.code,
+//       }, {
+//         headers: {
+//           'Content-Type': 'application/x-www-form-urlencoded'
+//         }
+//       })
+//       .then(resData => {
+//         return `Access Token: ${resData.data.access_token}`;
+//       })
+//       .catch(error => {
+//         console.log(error, '-- error 109 ----');
+//         return "Error fetching access token";
+//       })
 
-  } catch (error) {
-    console.log(error, ' ---- error 84 ---');
-    return res.status(500).send('Something Went Wrong!');
-  }
-})
+//   } catch (error) {
+//     console.log(error, ' ---- error 84 ---');
+//     return res.status(500).send('Something Went Wrong!');
+//   }
+// })
 
 // const dirPath = 'C:\\';
+
+
+
+app.get('/instaCallback',async (req, res) => {
+  const { code } = req.query;
+
+  const tokenUrl = 'https://api.instagram.com/oauth/access_token';
+  const client_id = process.env.INSTA_APP_ID;
+  const client_secret = process.env.INSTA_APP_SECRET;
+  const grant_type = "authorization_code";
+  const redirect_uri = process.env.REDIRECT_URI;
+
+  const formData = new FormData();
+  formData.append("client_id", client_id);
+  formData.append("client_secret", client_secret);
+  formData.append("grant_type", grant_type);
+  formData.append("redirect_uri", redirect_uri);
+  formData.append("code", code);
+  let shortLivedToken;
+  let token;
+    const response = await axios.post(tokenUrl, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    shortLivedToken = response.data.access_token;
+  if (shortLivedToken) {
+
+      const client_secret = process.env.INSTA_APP_SECRET;
+      const longLivedTokenUrl = `https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=${client_secret}&access_token=${shortLivedToken}`;
+      const response = await axios.get(longLivedTokenUrl);
+      token = response.data
+  }
+  const userData = await axios.get(`https://graph.instagram.com/me?fields=id,username,media_count,account_type&access_token=${token?.access_token}`);
+  // token.user_id = userData?.data?.id
+  // const tokenData = await tables.PluginWatchAndShopUserToken.findOne({where:{user_id:user_id,store_id:restaurant_id,type:"INSTAGRAM"}})
+  // if(tokenData){
+  //   await tokenData.update({token: token});
+  // }else{
+  //     await tables.PluginWatchAndShopUserToken.create({user_id:user_id,store_id:restaurant_id,token: token, type:"INSTAGRAM"});
+  // }
+  return res.send({
+    status:"Success",
+    status_code:200,
+    message:"token saved successfully",
+    data: userData
+  })
+});
+
 
 const options = {
   key: fs.readFileSync(path.join(__dirname,'/src/files/server.key')),
